4f3e348dadfbdfa7dbaed899ade8d701445c0bb8
Darsh Vora <darshvora0@gmail.com>
2020-01-03 08:50:02 +0530

    Added partial data access interface.

---
+++ Data/IDataAccessor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectStockManagement.Data
+{
+    public interface IDataAccessor
+    {
+        /// <summary>
+        /// Add breaking force
+        /// </summary>
+        /// <param name="breakingForce"></param>
+        void AddBreakingForce(int breakingForce);
+
+        /// <summary>
+        /// Adds GSM
+        /// </summary>
+        /// <param name="GSM"></param>
+        void AddGSM(int GSM);
+
+        /// <summary>
+        /// Gets list of breaking force 
+        /// </summary>
+        /// <returns></returns>
+        IList<int> GetBreakingForceList();
+
+        /// <summary>
+        /// Gets list of GSM
+        /// </summary>
+        /// <returns></returns>
+        IList<int> GetGSMList();
+
+        void AddClient(Client client);
+
+        void AddVehicle(Vehicle vehicle);
+
+        void AddOrder(Order order);
+
+        void AddOrders(IList<Order> orders);
+
+        void AddStock(Stock stock);
+
+        void AddStocks(IList<Stock> stocks);
+    }
+}
